ab74ec63ade193e6b1245d87a2b325b0418fc8d3
erdenetsogt <a.erdenetsogt@gmail.com>
2019-05-20 12:20:19 +0800

    mysql

--- Mysql.sql
+++ Mysql.sql
@@ -10,7 +10,11 @@ create table menu (
     ordering integer default 0,
     link text,
     created datetime not null,
-    primary key(id)
+    primary key(id),
+
+    key(name),
+    index(ordering)
+
 );
 
 
@@ -28,10 +32,62 @@ drop table menu;
 --------
 
 insert into menu values(null,"Politics",null,null,sysdate());
-insert into menu(id,name) values(null,"Entertainment");
+insert into menu(id,name,created) values(null,"Entertainment",sysdate());
 
 select * from menu;
 delete from menu;
 delete from menu where id=4;
 
 update menu set name='Entertainment' where id=3;
+
+--------
+
+select * from menu order by id;
+select * from menu order by id asc;
+
+select * from menu order by id desc;
+
+select * from menu order by ordering asc, name desc;
+
+
+------
+
+select count(*) from menu where ordering=0;
+select min(id) from menu;
+select max(id) from menu;
+select avg(id) from menu;
+select sum(id) from menu;
+
+--------
+create table category (
+    id integer auto_increment,
+    name varchar(200) not null,
+    ordering integer default 0,
+    primary key(id),
+    key(ordering)    
+);
+
+insert into category values(null,"Politics",1);
+insert into category values(null,"Entertainment",3);
+insert into category values(null,"Economy",2);
+
+create table content (
+    id integer auto_increment,
+    name varchar(200) not null,
+    created datetime not null,
+    text text,
+    category_id integer,
+    primary key(id),
+    foreign key (category_id) references category(id)    
+);
+
+insert into content values(null,"Uls toriing medee",sysdate(), "",1);
+insert into content values(null,"Uls toriing medee",sysdate(), "",null);
+
+
+select * from content left join category on content.category_id = category.id;
+
+
+
+
+
